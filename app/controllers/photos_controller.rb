85fb9ce6283219dba8c10cc2c56b288396326a25
tmtocb <kittz.esport@gmail.com>
2020-12-22 04:08:09 +0100

    allow only index, show Photos for not authenticated users

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,4 +1,5 @@
 class PhotosController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
   before_action :set_photo, only: [:show, :edit, :update, :destroy]
 
   # GET /photos
